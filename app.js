5cf9845bb74562de030c9e6d435bb84644f654af
JC <go4web@proton.me>
2023-07-20 20:34:14 +0800

    Seat Chart step 1 - Basic UI completed

--- app.js
+++ app.js
@@ -89,10 +89,13 @@ app.get("/demo", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("Contact");
 });
+//todo = Joe : select calendar days => searchart => booking
 app.get("/calendar", (req, res) => {
-  res.render("Calendar");
+  res.render("Calendar"); //calendar sandbox
 });
-
+app.get("/seatchart", (req, res) => {
+  res.render("Seatchart"); //seating plan sandbox
+});
 //! middleware
 app.use("/ideas", ensureAuthenticated, ideasRoute); //? main route
 app.use("/users", usersRoute); //? main route
